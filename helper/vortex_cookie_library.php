69516202df2503d9587270b7449c5c75a0f7fd18
DESKTOP-2LGC4LI\Indrajit Sengupta <is@woano.com>
2018-03-16 12:54:29 +0530

    Complete COOKIE library & some modifications in SESSION  library.

--- helper/vortex_cookie_library.php
+++ helper/vortex_cookie_library.php
@@ -8,7 +8,155 @@
 
 	class vortex_cookie_library{
 
-		
+		public static function cookie_flush($var_val_array){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_'])){
+
+				if(isset($_COOKIE["_-_-_cookie_flush_-_-_"])){
+					foreach($_COOKIE["_-_-_cookie_flush_-_-_"] as $key=>$val){
+						$data[$key] = $val;
+					}
+				}
+
+				foreach($var_val_array as $key=>$val){
+					$data[$key] = $val;
+				}
+
+				setcookie('_-_-_cookie_flush_-_-_', $data, time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+				setcookie("_-_-_vortex_cookie_id_-_-_", $_COOKIE['_-_-_vortex_cookie_id_-_-_'], time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+				return true;
+			}
+			else
+				return false;
+		}
+
+		public static function get_flush_value($variable){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_'])){
+				if(isset($_COOKIE["_-_-_cookie_flush_-_-_"])){
+					if(isset($_COOKIE["_-_-_cookie_flush_-_-_"][$variable])){
+						vortex_cookie_library::destroy_flush_variable($variable);
+						setcookie("_-_-_vortex_cookie_id_-_-_", $_COOKIE['_-_-_vortex_cookie_id_-_-_'], time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+						return $_COOKIE["_-_-_cookie_flush_-_-_"][$variable];
+					}
+					else
+						return null;
+				}
+				else
+					return null;
+			}
+			else
+				return null;
+		}
+
+		public static function destroy_flush_variable($variable){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_'])){
+				if(isset($_COOKIE["_-_-_cookie_flush_-_-_"])){
+					if(isset($_COOKIE["_-_-_cookie_flush_-_-_"][$variable]))
+						unset($_COOKIE["_-_-_cookie_flush_-_-_"][$variable]);
+				}
+			}
+
+			return true;
+		}
+
+		public static function cookie_init($push_data){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_']))
+				vortex_cookie_library::destroy_cookies();
+
+			$data_string = uniqid('jgu38fr9cftj34ct7t8fcr894', true)."".time()."".date("YMHmhisaugG")."".rand(100000, 999999);
+			foreach($push_data as $key=>$val){
+				$data_string .= $val;
+			}
+
+			$uniqid = sha1(str_shuffle($data_string));
+			setcookie("_-_-_vortex_cookie_id_-_-_", $uniqid, time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+
+			foreach($push_data as $key=>$val){
+				setcookie($key, $val, time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+			}
+
+			return true;
+		}
+
+		public static function cookie_push($var_array){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_'])){
+				foreach($var_array as $key=>$val){
+					setcookie($key, $val, time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+				}
+				setcookie("_-_-_vortex_cookie_id_-_-_", $_COOKIE['_-_-_vortex_cookie_id_-_-_'], time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+				return true;
+			}
+			else
+				return false;
+		}
+
+		public static function cookie_pop($key_array){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_'])){
+				foreach($key_array as $key=>$val){
+					if(isset($_COOKIE[$key]))
+						unset($_COOKIE[$key]);
+				}
+				setcookie("_-_-_vortex_cookie_id_-_-_", $_COOKIE['_-_-_vortex_cookie_id_-_-_'], time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+				return true;
+			}
+			else
+				return false;
+		}
+
+		public static function destroy_cookies(){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_'])){
+				unset($_COOKIE);
+	    	}
+
+	    	return true;
+		}
+
+		public static function get_cookie_ID(){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_']))
+				return $_COOKIE['_-_-_vortex_cookie_id_-_-_'];
+	    	else
+	    		return null;
+		}
+
+		public static function modify_cookie_data($modified_data_array){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_'])){
+				foreach($modified_data_array as $key=>$val){
+					setcookie($key, $val, time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+				}
+				setcookie("_-_-_vortex_cookie_id_-_-_", $_COOKIE['_-_-_vortex_cookie_id_-_-_'], time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+				return true;
+			}
+			else
+				return false;
+		}
+
+		public static function get_all_cookie_data(){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_'])){
+				$data = array();
+				foreach($_COOKIE as $key=>$val){
+					if($key!='_-_-_vortex_cookie_id_-_-_' && $key!='_-_-_cookie_flush_-_-_')
+						$data[$key] = $val;
+				}
+
+				setcookie("_-_-_vortex_cookie_id_-_-_", $_COOKIE['_-_-_vortex_cookie_id_-_-_'], time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+
+				return $data;
+			}
+			else
+				return null;
+		}
+
+		public static function get_cookie_data($variable){
+			if(isset($_COOKIE['_-_-_vortex_cookie_id_-_-_'])){
+				setcookie("_-_-_vortex_cookie_id_-_-_", $_COOKIE['_-_-_vortex_cookie_id_-_-_'], time()+$GLOBALS['_-_-_manifest_-_-_']['_-_-_COOKIE_expiration_time_-_-_']);
+
+				if(isset($_COOKIE[$variable]))
+					return $_COOKIE[$variable];
+				else
+					return null;
+			}
+			else
+				return null;
+		}
 		
 	}
 
--- helper/vortex_session_library.php
+++ helper/vortex_session_library.php
@@ -11,16 +11,26 @@
 
 	class vortex_session_library{
 
+		// MAKE FILE ONLY READABLE WITH SESSION TERMINATION
+
 		public static function session_flush($variable, $value){
-			$_SESSION['_-_-_vortex_flush_-_-_'][$variable] = $value;
-			return true;
+			if(isset($_SESSION['_-_-_init_time_-_-_'])){
+				$_SESSION['_-_-_vortex_flush_-_-_'][$variable] = $value;
+				return true;
+			}
+			else
+				return false;
 		}
 
 		public static function get_flush_variable($variable){
-			if(isset($_SESSION['_-_-_vortex_flush_-_-_'][$variable])){
-				$flush_variable = $_SESSION['_-_-_vortex_flush_-_-_'][$variable];
-				vortex_session_library::destroy_flush_variable($variable);
-				return $flush_variable;
+			if(isset($_SESSION['_-_-_init_time_-_-_'])){
+				if(isset($_SESSION['_-_-_vortex_flush_-_-_'][$variable])){
+					$flush_variable = $_SESSION['_-_-_vortex_flush_-_-_'][$variable];
+					vortex_session_library::destroy_flush_variable($variable);
+					return $flush_variable;
+				}
+				else
+					return null;
 			}
 			else
 				return null;
@@ -32,59 +42,74 @@
 		}
 
 		public static function session_init($push_data){
-			$uid = session_id();
-			session_unset();
-			session_destroy();
+			if(isset($_SESSION['_-_-_init_time_-_-_']))
+				return false;
+			else{
+				$uid = session_id();
+				session_unset();
+				session_destroy();
 
-			$user_IP = vortex_user_info_library::getRealIpAddr();
+				$user_IP = vortex_user_info_library::getRealIpAddr();
 
-			$data_string = $uid . "" . $user_IP . "" . date('gsmMhHYuia') . "" . rand(100000, 999999);
+				$data_string = $uid . "" . $user_IP . "" . date('gsmMhHYuia') . "" . rand(100000, 999999);
 
-			foreach($push_data as $key=>$val){
-				$data_string .= $val;
+				foreach($push_data as $key=>$val){
+					$data_string .= $val;
+				}
+
+				$session_id = sha1(str_shuffle($data_string));
+				session_id($session_id);
+				session_start();
+				$current_time = microtime(true);
+				$_SESSION['_-_-_init_time_-_-_'] = $current_time;
+				$_SESSION['_-_-_last_activation_time_-_-_'] = $current_time;
+
+				$push_data['user_IP'] = $user_IP;
+
+				if($GLOBALS['_-_-_manifest_-_-_']['_-_-_record_session_-_-_'])
+					vortex_session_library::generate_session_record($session_id, $push_data);
+
+				return true;
 			}
-
-			$session_id = sha1(str_shuffle($data_string));
-			session_id($session_id);
-			session_start();
-			$current_time = microtime(true);
-			$_SESSION['_-_-_init_time_-_-_'] = $current_time;
-			$_SESSION['_-_-_last_activation_time_-_-_'] = $current_time;
-
-			$push_data['user_IP'] = $user_IP;
-
-			if($GLOBALS['_-_-_manifest_-_-_']['_-_-_record_session_-_-_'])
-				vortex_session_library::generate_session_record($session_id, $push_data);
-
-			return true;
 		}
 
 		public static function session_push($var_array){
-			if(isset($_SESSION)){
+			if(isset($_SESSION['_-_-_init_time_-_-_'])){
 				foreach($var_array as $key=>$val){
 					$_SESSION[$key] = $val;
 				}
+
+				return true;
 			}
+			else
+				return false;
 		}
 
 		public static function session_pop($key_array){
-			if(isset($_SESSION)){
+			if(isset($_SESSION['_-_-_init_time_-_-_'])){
 				foreach($key_array as $key){
 					unset($_SESSION[$key]);
 				}
+
+				return true;
 			}
+			else
+				return false;
 		}
 
 		public static function session_terminate(){
-			if(isset($_SESSION)){
+			if(isset($_SESSION['_-_-_init_time_-_-_'])){
 				$current_time = microtime(true);
 				if($GLOBALS['_-_-_manifest_-_-_']['_-_-_record_session_-_-_']){
 					vortex_session_library::modify_session_data([
-						"session_end"	=>	$current_time
+						"session_end"	=>	$current_time,
+						"termination_time"	=>	vortex_datetime_library::current_time('datetime_micro')
 					]);
 				}
+				unset($_SESSION);
 				session_unset();
 	    		session_destroy();
+
 	    	}
 
 	    	return true;
@@ -97,156 +122,189 @@
 				"session_valid"		=>	true
 			];
 
-			if($GLOBALS['_-_-_manifest_-_-_']['_-_-_SESSION_auto_expiration_-_-_']){
-				if($current_time-$_SESSION['_-_-_init_time_-_-_'] >= $GLOBALS['_-_-_manifest_-_-_']['_-_-_SESSION_expiration_time_-_-_']){
-					vortex_session_library::session_terminate();
+			if(isset($_SESSION['_-_-_init_time_-_-_'])){
 
-					$response = [
-						"success"			=>	0,
-						"session_valid"		=>	false
-					];
+				if($GLOBALS['_-_-_manifest_-_-_']['_-_-_SESSION_auto_expiration_-_-_']){
+					if($current_time-$_SESSION['_-_-_init_time_-_-_'] >= $GLOBALS['_-_-_manifest_-_-_']['_-_-_SESSION_expiration_time_-_-_']){
+						vortex_session_library::session_terminate();
+
+						$response = [
+							"success"			=>	0,
+							"session_valid"		=>	false
+						];
+					}
 				}
-			}
 
-			if($GLOBALS['_-_-_manifest_-_-_']['_-_-_SESSION_idle_expiration_-_-_']){
-				if($current_time-$_SESSION['_-_-_last_activation_time_-_-_'] >= $GLOBALS['_-_-_manifest_-_-_']['_-_-_SESSION_idle_expiration_time_-_-_']){
-					vortex_session_library::session_terminate();
+				if($GLOBALS['_-_-_manifest_-_-_']['_-_-_SESSION_idle_expiration_-_-_']){
+					if($current_time-$_SESSION['_-_-_last_activation_time_-_-_'] >= $GLOBALS['_-_-_manifest_-_-_']['_-_-_SESSION_idle_expiration_time_-_-_']){
+						vortex_session_library::session_terminate();
 
-					$response = [
-						"success"			=>	0,
-						"session_valid"		=>	false
-					];
+						$response = [
+							"success"			=>	0,
+							"session_valid"		=>	false
+						];
+					}
 				}
-			}
 
-			if($response['success']==1 && $response['session_valid'])
-				$_SESSION['_-_-_last_activation_time_-_-_'] = $current_time;
+				if($response['success']==1 && $response['session_valid'])
+					$_SESSION['_-_-_last_activation_time_-_-_'] = $current_time;
+			}
+			else{
+				$response = [
+					"success"			=>	0,
+					"session_valid"		=>	false
+				];
+			}
 
 			return $response;
 		}
 
 		public static function generate_session_record($session_id, $session_data){
-			$session_file_name = vortex_datetime_library::current_time('date') . "_" . $session_id;
-			$session_file_extension = $GLOBALS['_-_-_manifest_-_-_']['_-_-_session_file_extension_-_-_'];
-			$session_folder = 'storage/session/';
+			if(isset($_SESSION['_-_-_init_time_-_-_'])){
+				$session_file_name = vortex_datetime_library::current_time('date') . "_" . $session_id;
+				$session_file_extension = $GLOBALS['_-_-_manifest_-_-_']['_-_-_session_file_extension_-_-_'];
+				$session_folder = 'storage/session/';
 
-			$current_time = vortex_datetime_library::current_time('time_micro');
+				$current_time = vortex_datetime_library::current_time('time_micro');
 
-			chdir($GLOBALS['_-_-_manifest_-_-_']['_-_-_DOCUMENT_ROOT_-_-_']);
-			if(!file_exists($session_folder))
-	            mkdir($session_folder, 0777, true);
-	        if(!file_exists($session_folder . "" . $session_file_name . "" . $session_file_extension)){
-	        	$handle = fopen($session_folder . "" . $session_file_name . "" . $session_file_extension, 'w');
-	        	fclose($handle);
-	        }
+				chdir($GLOBALS['_-_-_manifest_-_-_']['_-_-_DOCUMENT_ROOT_-_-_']);
+				if(!file_exists($session_folder))
+		            mkdir($session_folder, 0777, true);
+		        if(!file_exists($session_folder . "" . $session_file_name . "" . $session_file_extension)){
+		        	$handle = fopen($session_folder . "" . $session_file_name . "" . $session_file_extension, 'w');
+		        	fclose($handle);
+		        }
 
-	        $data = parse_ini_file($session_folder . "" . $session_file_name . "" . $session_file_extension, true);
+		        $data = parse_ini_file($session_folder . "" . $session_file_name . "" . $session_file_extension, true);
 
-	        $session_string = "[".$session_id."]\n";
-	        $session_string .= "session_start=".$_SESSION['_-_-_init_time_-_-_']."\n";
-	        // $session_string .= "session_last_activated=".$_SESSION['_-_-_last_activation_time_-_-_']."\n";
+		        $session_string = "[".$session_id."]\n";
+		        $session_string .= "session_start=".$_SESSION['_-_-_init_time_-_-_']."\n";
+		        // $session_string .= "session_last_activated=".$_SESSION['_-_-_last_activation_time_-_-_']."\n";
 
-	        foreach($session_data as $key=>$val){
-	        	$session_string .= $key."=".$val."\n";
-	        }
-
-	        if($GLOBALS['_-_-_manifest_-_-_']['_-_-_fetch_user_info_-_-_']){
-	        	$user_info = vortex_session_library::user_info();
-	        	foreach($user_info as $key=>$val){
+		        foreach($session_data as $key=>$val){
 		        	$session_string .= $key."=".$val."\n";
 		        }
-	        }
 
-	        $session_string .= "\n\n";
+		        if($GLOBALS['_-_-_manifest_-_-_']['_-_-_fetch_user_info_-_-_']){
+		        	$user_info = vortex_session_library::user_info();
+		        	foreach($user_info as $key=>$val){
+			        	$session_string .= $key."=".$val."\n";
+			        }
+		        }
 
-	        $session_string = str_replace(";", "", $session_string);
-	        $session_string = str_replace("(", "", $session_string);
-	        $session_string = str_replace(")", "", $session_string);
+		        $session_string .= "timezone"."=".vortex_datetime_library::get_DateTimeZone()."\n";
+		        $session_string .= "start_time"."=".vortex_datetime_library::current_time('datetime_micro')."\n";
 
-	        file_put_contents($session_folder . "" . $session_file_name . "" . $session_file_extension, $session_string, LOCK_EX);
+		        $session_string .= "\n\n";
 
-	        return true;
+		        $session_string = str_replace(";", "", $session_string);
+		        $session_string = str_replace("(", "", $session_string);
+		        $session_string = str_replace(")", "", $session_string);
+
+		        file_put_contents($session_folder . "" . $session_file_name . "" . $session_file_extension, $session_string, LOCK_EX);
+
+		        return true;
+
+		    }
+		    else
+		    	return false;
+		    
 		}
 
 		public static function get_session_ID(){
 			$session_id = null;
-			if(isset($_SESSION))
+			if(isset($_SESSION['_-_-_init_time_-_-_']))
 				$session_id = session_id();
 
 			return $session_id;
 		}
 
 		public static function modify_session_data($modified_data){
-			$session_id = vortex_session_library::get_session_ID();
-			$session_file_name = vortex_datetime_library::current_time('date') . "_" . $session_id;
-			$session_file_extension = $GLOBALS['_-_-_manifest_-_-_']['_-_-_session_file_extension_-_-_'];
-			$session_folder = 'storage/session/';
-			$session_data = [];
+			if(isset($_SESSION['_-_-_init_time_-_-_'])){
+				$session_id = vortex_session_library::get_session_ID();
+				$session_file_name = vortex_datetime_library::current_time('date') . "_" . $session_id;
+				$session_file_extension = $GLOBALS['_-_-_manifest_-_-_']['_-_-_session_file_extension_-_-_'];
+				$session_folder = 'storage/session/';
+				$session_data = [];
 
-			chdir($GLOBALS['_-_-_manifest_-_-_']['_-_-_DOCUMENT_ROOT_-_-_']);
-			if(!file_exists($session_folder))
-	            mkdir($session_folder, 0777, true);
-	        if(!file_exists($session_folder . "" . $session_file_name . "" . $session_file_extension)){
-	        	$handle = fopen($session_folder . "" . $session_file_name . "" . $session_file_extension, 'w');
-	        	fclose($handle);
-	        }
+				chdir($GLOBALS['_-_-_manifest_-_-_']['_-_-_DOCUMENT_ROOT_-_-_']);
+				if(!file_exists($session_folder))
+		            mkdir($session_folder, 0777, true);
+		        if(!file_exists($session_folder . "" . $session_file_name . "" . $session_file_extension)){
+		        	$handle = fopen($session_folder . "" . $session_file_name . "" . $session_file_extension, 'w');
+		        	fclose($handle);
+		        }
 
-	        $data = parse_ini_file($session_folder . "" . $session_file_name . "" . $session_file_extension, true);
-	        $session_id_found = 0;
+		        $data = parse_ini_file($session_folder . "" . $session_file_name . "" . $session_file_extension, true);
+		        $session_id_found = 0;
 
-	        foreach($data as $key=>$val){
-	        	if($key == $session_id){
-	        		$session_id_found = 1;
+		        foreach($data as $key=>$val){
+		        	if($key == $session_id){
+		        		$session_id_found = 1;
 
-	        		foreach($val as $val_key=>$val_val){
-	        			$index_data_found = 0;
-	        			$index_data_count = 0;
-	        			$key_val_array = [];
-	        			foreach($modified_data as $modified_data_key=>$modified_data_val){
-	        				$index_data_count++;
-	        				$key_val_array = [
-	        					"key"	=>	$modified_data_key,
-	        					"val"	=>	$modified_data_val
-	        				];
-	        				if($val_key == $modified_data_key){
-	        					$session_data[$val_key] = $modified_data_val;
-	        					$index_data_found = 1;
-	        					break;
+		        		foreach($val as $val_key=>$val_val){
+		        			$index_data_found = 0;
+		        			$index_data_count = 0;
+		        			$key_val_array = [];
+		        			foreach($modified_data as $modified_data_key=>$modified_data_val){
+		        				$index_data_count++;
+		        				$key_val_array = [
+		        					"key"	=>	$modified_data_key,
+		        					"val"	=>	$modified_data_val
+		        				];
+		        				if($val_key == $modified_data_key){
+		        					$session_data[$val_key] = $modified_data_val;
+		        					$index_data_found = 1;
+		        					break;
+		        				}
+		        			}
+		        			if($index_data_found==0 && $index_data_count>0){
+	        					$session_data[$key_val_array['key']] = $key_val_array['val'];
 	        				}
-	        			}
-	        			if($index_data_found==0 && $index_data_count>0){
-        					$session_data[$key_val_array['key']] = $key_val_array['val'];
-        				}
-	        		}
-	        	}
+		        		}
+		        	}
 
-	        	if($session_id_found == 1)
-	        		break;
-	        }
+		        	if($session_id_found == 1)
+		        		break;
+		        }
 
-	        if(sizeof($session_data) > 0){
-	        	foreach($session_data as $key=>$val){
-	        		$data[$session_id][$key] = $val;
-	        	}
-	        }
+		        if(sizeof($session_data) > 0){
+		        	foreach($session_data as $key=>$val){
+		        		$data[$session_id][$key] = $val;
+		        	}
+		        }
 
-	        ksort($data[$session_id]);
+		        ksort($data[$session_id]);
 
-	        $session_string = "[".$session_id."]\n";
+		        $session_string = "[".$session_id."]\n";
 
-	        foreach($data[$session_id] as $key=>$val){
-	        	$session_string .= $key."=".$val."\n";
-	        }
+		        foreach($data[$session_id] as $key=>$val){
+		        	$session_string .= $key."=".$val."\n";
+		        }
 
-	        $session_string .= "\n\n";
+		        $session_string .= "\n\n";
 
-	        $session_string = str_replace(";", "", $session_string);
-	        $session_string = str_replace("(", "", $session_string);
-	        $session_string = str_replace(")", "", $session_string);
+		        $session_string = str_replace(";", "", $session_string);
+		        $session_string = str_replace("(", "", $session_string);
+		        $session_string = str_replace(")", "", $session_string);
 
-	        file_put_contents($session_folder . "" . $session_file_name . "" . $session_file_extension, $session_string, LOCK_EX);
+		        file_put_contents($session_folder . "" . $session_file_name . "" . $session_file_extension, $session_string, LOCK_EX);
 
-	        return true;
+		        return true;
+		    }
+		    else
+		    	return false;
+		}
+
+		public static function get_session_data($variable){
+			if(isset($_SESSION['_-_-_init_time_-_-_'])){
+				if(isset($_SESSION[$variable]))
+					return $_SESSION[$variable];
+				else
+					return null;
+			}
+			else
+				return null;
 		}
 
 		public static function user_info(){
